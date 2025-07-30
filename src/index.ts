cc0bb6ac3d3edf255af4f8671764e49b94ff9aeb
ncpa0cpl <szymonb21@gmail.com>
2025-07-30 15:58:04 +0200

    fix: input suggestion selection

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 export * from "./components/input/input";
+export type {
+  AdwSelectorOption,
+  AdwSelectorOptionAttributes,
+  AdwSelectorOptionEvents,
+} from "./components/selector/option";
 export * from "./components/selector/selector";
 export * from "./components/slider/slider";
 export * from "./components/switch/switch";
